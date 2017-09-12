d0cf95780589f213d22b845eeb602c1ce771d5f6
Dvir Volk <dvirsky@gmail.com>
2017-09-12 17:07:43 +0300

    ignore import error with old versions of requests

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='rmadm',
-    version='0.1',
+    version='0.1.1',
     description='Command-line control tool for redis modules Redis Labs Enterprise Cluster',
     author='RedisLabs',
     url='https://github.com/redislabs/rmadm',
     scripts=['rmadm/rmadm'],
     license='BSD 2-clause',
     packages=find_packages(),
-    install_requires=['click', 'requests']
+    install_requires=['click', 'requests', 'colorama']
 )
